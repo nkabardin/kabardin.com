a6a9a6a6af2737182d9d27901b91d583ad44178f
Nikita Kabardin <nikita@kabardin.com>
2017-05-28 14:10:50 +0200

    :sparkles: New version of website with create-react-app

---
+++ website/src/App.js
@@ -0,0 +1,306 @@
+import React, {Component} from 'react';
+import 'normalize.css/normalize.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="content">
+        <div className="block is-main">
+          <p>Hello, my name is</p>
+          <p className="focus">Nikita Kabardin</p>
+        </div>
+
+        <div className="block">
+          <p>
+            I'm a software developer from
+            <span className="item">
+              <span role="img" aria-label="">🇷🇺</span>
+              Russia
+            </span> recently moved to
+            <span className="item">
+              <span role="img" aria-label="">🇸🇪</span>
+              Stockholm
+            </span>.
+          </p>
+        </div>
+
+        <div className="block">
+          <p>
+            I work at {' '}
+            <a href="http://0x.se">0+X</a>
+            {' '}
+            as a front-end development{' '}
+            <span className="item">
+              <span role="img" aria-label="">🕵️</span>consultant
+            </span>
+            , helping companies
+            to build modern browser-based applications.
+            You can check out my{' '}
+            <a href="https://www.linkedin.com/in/nkabardin/">LinkedIn</a>{' '}
+            to find out more about what I do.
+          </p>
+        </div>
+
+        <div className="block">
+          <p>
+            As a hobby, I do
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="">🎼</span> weird music
+            </span>
+            {' '}
+            and
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="">💩</span>sound art
+            </span>
+            . Check out some of my
+            <span className="item">
+              <span role="img" aria-label="">🎹</span>
+              <a href="http://soundcloud.com/nkabardin">recordings</a>
+            </span>
+            {' '}
+            or
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="">🎥</span><a
+                href="https://www.youtube.com/watch?v=QGqfX-wFNVE"
+              >
+                video
+              </a>
+            </span>
+            {' '}of me playing my favourite instrument —
+            <span className="item">
+              <span role="img" aria-label="">🥁</span>Roland HPD-20
+            </span>
+            . Or,
+            if you're into electronic music — you may like our improvized electronic
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="">🎛</span><a
+                href="https://www.youtube.com/watch?v=QHlbtasdS_0"
+              >
+                live set
+              </a>
+            </span>
+            .
+          </p>
+        </div>
+
+        <div className="block">
+          <p>
+            My other hobbies include
+            <span className="item">
+              <span role="img" aria-label="tent">🏕</span>hiking
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="bicycle">🚴</span>cycling
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="ski">⛷</span>alpine ski
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="hockey stick">🏒</span>ice hockey
+            </span>
+            {' '}
+            and
+            {' '}
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="chili pepper">🌶</span>spicy food
+            </span>
+            .
+          </p>
+        </div>
+
+        <div className="block">
+          <p>
+            Of course, I love to
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="plane">✈️</span> travel
+            </span>
+            ! So far I've been to
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Austria flag">🇦🇹</span>Austria
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Belarus flag">🇧🇾</span>Belarus
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="China flag">🇨🇳</span>China
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Czech Republic flag">🇨🇿</span>
+              Czech Republic
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Germany flag">🇩🇪</span>Germany
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Denmark flag">🇩🇰</span>Denmark
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Egypt flag">🇪🇬</span>Egypt
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Spain flag">🇪🇸</span>Spain
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Finland flag">🇫🇮</span>Finland
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="France flag">🇫🇷</span>France
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Hong Kong flag">🇭🇰</span>Hong Kong
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Indonesia flag">🇮🇩</span>Indonesia
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Italy flag">🇮🇹</span>Italy
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Latvia flag">🇱🇻</span>Latvia
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Malta flag">🇲🇹</span>Malta
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Malaysia flag">🇲🇾</span>Malaysia
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="The Netherlands flag">🇳🇱</span>
+              The Netherlands
+            </span>
+            .
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Portugal flag">🇵🇹</span>Portugal
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Russia flag">🇷🇺</span>Russia
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Sweden flag">🇸🇪</span>Sweden
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Singapore flag">🇸🇬</span>Singapore
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Slovakia flag">🇸🇰</span>Slovakia
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Thailand flag">🇹🇭</span>Thailand
+            </span>
+            ,
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="Ukraine flag">🇺🇦</span>Ukraine
+            </span>
+            .
+            {' '}
+          </p>
+        </div>
+
+        <div className="block">
+          <p>
+            Follow me on
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="">🖼</span><a
+                href="http://instagram.com/nkabardin"
+              >
+                Instagram
+              </a>
+            </span>
+
+            . Sometimes I post
+            interesting stuff there.
+          </p>
+        </div>
+
+        <div className="block">
+          <p>
+            I
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="">❤️</span>
+            </span>
+            {' '}
+            to make friends. Friend me on
+            {' '}
+            <a href="http://facebook.com/nkabardin">Facebook</a>.
+          </p>
+        </div>
+
+        <div className="block">
+          <p>
+            Or just drop me a line to
+            {' '}
+            <span className="item">
+              <span role="img" aria-label="">📧</span><a
+                href="mailto:nikita@kabardin.com"
+              >
+                nikita@kabardin.com
+              </a>
+            </span>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
